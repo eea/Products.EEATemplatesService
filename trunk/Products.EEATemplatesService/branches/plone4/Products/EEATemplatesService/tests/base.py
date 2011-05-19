ed13470bafaca7fc5ebac5132ac1da2df59b9fdb
ichimdav <ichimdav@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-19 08:44:17 +0000

    Refs #8297; modified test base to pick up correct testfixture profile; added todo's in HISTORY for this package

--- trunk/Products.EEATemplatesService/branches/plone4/Products/EEATemplatesService/tests/base.py
+++ trunk/Products.EEATemplatesService/branches/plone4/Products/EEATemplatesService/tests/base.py
@@ -7,7 +7,7 @@ from Products.Five import fiveconfigure
 from Products.GenericSetup import EXTENSION, profile_registry
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 
-PRODUCTS = ['CacheSetup', 'EEATemplatesService']
+PRODUCTS = ['CacheSetup', 'collective.monkeypatcher',  'EEATemplatesService']
 
 profile_registry.registerProfile(
                     'testfixture',
@@ -25,20 +25,23 @@ def setup_eeacontenttypes():
     """ setup_eeacontenttypes test setup function """
     fiveconfigure.debug_mode = True
     import Products.Five
+    import collective.monkeypatcher
     import Products.CMFSquidTool
     zcml.load_config('meta.zcml', Products.Five)
+    zcml.load_config('meta.zcml', collective.monkeypatcher)
     zcml.load_config('configure.zcml', Products.CMFSquidTool)
+    PloneTestCase.installProduct('Five')
+    PloneTestCase.installProduct('colective.monkeypatcher')
+    PloneTestCase.installProduct('CMFSquidTool')
     fiveconfigure.debug_mode = False
 
-    PloneTestCase.installProduct('Five')
-    PloneTestCase.installProduct('CMFSquidTool')
     for product in PRODUCTS:
         PloneTestCase.installProduct(product)
 
 
 setup_eeacontenttypes()
-PloneTestCase.setupPloneSite(extension_profiles=['EEATemplatesService:default',
-                        'EEATemplatesService:testfixture'], products=PRODUCTS)
+PloneTestCase.setupPloneSite(extension_profiles=['Products.EEATemplatesService:default',
+  'Products.EEATemplatesService:testfixture'   ], products=PRODUCTS)
 
 class EEATemplatesService(PloneTestCase.FunctionalTestCase):
     """ EEATemplatesService FunctionalTestCase class """

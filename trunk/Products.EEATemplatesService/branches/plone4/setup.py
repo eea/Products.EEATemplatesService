5f072a741840aa836bb2fe9939b3f0812eb7d0d6
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-05-20 08:48:48 +0000

    Fixed Products.EEATemplatesService installer, refs #8297

--- trunk/Products.EEATemplatesService/branches/plone4/setup.py
+++ trunk/Products.EEATemplatesService/branches/plone4/setup.py
@@ -8,34 +8,34 @@ PATH = NAME.split('.') + ['version.txt']
 VERSION = open(os.path.join(*PATH)).read().strip()
 
 setup(
- name=NAME,
- version=VERSION,
- description="EEATemplatesService is a products that provides simple http "
-             "services for the templates of a plone site",
- long_description=open("README.txt").read() + "\n" +
-                  open(os.path.join("docs", "HISTORY.txt")).read(),
- url="https://svn.eionet.europa.eu/projects/"
-     "Zope/browser/trunk/Products.EEATemplatesService",
- classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-   ],
- keywords='EEATemplatesService',
- author='Sasha Vincic (EEA), European Environment Agency',
- author_email='webadmin@eea.europa.eu',
- license='GPL',
- packages=find_packages(exclude=['ez_setup']),
- namespace_packages=['Products'],
- include_package_data=True,
- zip_safe=False,
- install_requires=[
-     "setuptools",
-     "collective.fastview",
-     "collective.monkeypatcher"
- ],
- entry_points="""
- # -*- Entry points: -*-
- """,
+    name=NAME,
+    version=VERSION,
+    description="EEATemplatesService is a products that provides simple http "
+                "services for the templates of a plone site",
+    long_description=open("README.txt").read() + "\n" +
+                     open(os.path.join("docs", "HISTORY.txt")).read(),
+    url="https://svn.eionet.europa.eu/projects/"
+        "Zope/browser/trunk/Products.EEATemplatesService",
+    classifiers=[
+           "Framework :: Plone",
+           "Programming Language :: Python",
+           "Topic :: Software Development :: Libraries :: Python Modules",
+      ],
+    keywords='EEATemplatesService',
+    author='Sasha Vincic (EEA), European Environment Agency',
+    author_email='webadmin@eea.europa.eu',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['Products'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        "collective.fastview",
+        "collective.monkeypatcher"
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
  )
 

8e290fa84a6322ca64f4c06acd954a5447eb9217
tiberich <tiberiuichim@users.noreply.github.com>
2011-11-14 16:06:20 +0000

    Refs #8677; Move current trunk to plone25 branch

---
+++ Products/EEATemplatesService/Extensions/Install.py
@@ -0,0 +1,17 @@
+from cStringIO import StringIO
+
+#from Products.EEATemplatesService.Extensions.utils import *
+#from Products.EEATemplatesService.config import *
+
+def install(self):
+    out = StringIO()
+
+    print >> out, "Installation completed."
+    return out.getvalue()
+
+
+def uninstall(self):
+    out = StringIO()
+    return out.getvalue()
+
+

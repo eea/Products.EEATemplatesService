ea4144eab7c1e40f43c2ad0e12785c372e9fc7fa
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-11-18 13:45:31 +0000

    Tagged Products.EEATemplatesService 4.0.

---
+++ trunk/Products.EEATemplatesService/tags/4.0/Products/EEATemplatesService/skins/eeatemplatesservice/getFooter.py
@@ -0,0 +1,10 @@
+## Script (Python) "getFooter"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=site='default'
+##title=Returns  footer for the website
+
+return context.eea_footer()

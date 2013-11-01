bf9f10ba9943a6162319cfd137119e0b7aa75900
simiamih <simiamih@24821446-e7ea-0310-81ac-b189f4507b0b>
2013-11-01 14:17:36 +0000

    patch for losing css/js registry on submit after ac cookie expired refs #17259

--- Products/EEATemplatesService/patches.py
+++ Products/EEATemplatesService/patches.py
@@ -22,7 +22,14 @@ def manage_saveStylesheets(self, REQUEST=None):
     """ Save stylesheets from the ZMI.
         Updates the whole sequence. For editing and reordering.
     """
+    # 1. first, a temporary patch for #17259
+    if REQUEST and not REQUEST.form:
+        REQUEST.RESPONSE.redirect("%s/manage_workspace" %
+                                  self.absolute_url_path())
+        return
+    # 2. original call:
     self._old_manage_saveStylesheets(REQUEST)
+    # 3. and now our permanent patch:
     invalidateClientsCache(self, REQUEST)
 
 security.declareProtected(permissions.ManagePortal, 'manage_saveScripts')
@@ -30,5 +37,12 @@ def manage_saveScripts(self, REQUEST=None):
     """ Save scripts from the ZMI.
         Updates the whole sequence. For editing and reordering.
     """
+    # 1. first, a temporary patch for #17259
+    if REQUEST and not REQUEST.form:
+        REQUEST.RESPONSE.redirect("%s/manage_workspace" %
+                                  self.absolute_url_path())
+        return
+    # 2. original call:
     self._old_manage_saveScripts(REQUEST)
+    # 3. and now our permanent patch:
     invalidateClientsCache(self, REQUEST)

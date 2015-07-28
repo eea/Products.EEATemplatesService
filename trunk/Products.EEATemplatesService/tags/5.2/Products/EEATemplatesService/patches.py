36a0c21c9785e23d1ff3df00e0ce59f3290a28fd
ghicaale <ghicaale@24821446-e7ea-0310-81ac-b189f4507b0b>
2015-07-28 10:43:20 +0000

    Tagged Products.EEATemplatesService 5.2.

---
+++ trunk/Products.EEATemplatesService/tags/5.2/Products/EEATemplatesService/patches.py
@@ -0,0 +1,46 @@
+""" Patches to resource registries
+"""
+from AccessControl import ClassSecurityInfo
+from Products.CMFCore.utils import getToolByName
+from Products.ResourceRegistries import permissions
+from zope.lifecycleevent import ObjectModifiedEvent
+from zope.event import notify
+
+security = ClassSecurityInfo()
+
+def invalidateClientsCache(self, REQUEST=None):
+    """ Invalidate client cache
+    """
+    notify(ObjectModifiedEvent(self))
+    if REQUEST:
+        portal_url = getToolByName(self, 'portal_url')()
+        return REQUEST.RESPONSE.redirect(portal_url +
+                '/@@invalidateClientsCache')
+
+security.declareProtected(permissions.ManagePortal, 'manage_saveStylesheets')
+def manage_saveStylesheets(self, REQUEST=None):
+    """ Save stylesheets from the ZMI.
+        Updates the whole sequence. For editing and reordering.
+    """
+    # 1. first, a temporary patch for #17259
+    if REQUEST and not REQUEST.form:
+        REQUEST.RESPONSE.redirect("manage_workspace")
+        return
+    # 2. original call:
+    self._old_manage_saveStylesheets(REQUEST)
+    # 3. and now our permanent patch:
+    invalidateClientsCache(self, REQUEST)
+
+security.declareProtected(permissions.ManagePortal, 'manage_saveScripts')
+def manage_saveScripts(self, REQUEST=None):
+    """ Save scripts from the ZMI.
+        Updates the whole sequence. For editing and reordering.
+    """
+    # 1. first, a temporary patch for #17259
+    if REQUEST and not REQUEST.form:
+        REQUEST.RESPONSE.redirect("manage_workspace")
+        return
+    # 2. original call:
+    self._old_manage_saveScripts(REQUEST)
+    # 3. and now our permanent patch:
+    invalidateClientsCache(self, REQUEST)

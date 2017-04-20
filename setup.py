631d457b9b154105edb814f35ebbebd7f21d25f2
eduard-fironda <eduard.fironda@eaudeweb.ro>
2017-04-20 13:57:44 +0300

    updated package information
    * Change: updated package information
      [eduard-fironda]

--- setup.py
+++ setup.py
@@ -14,16 +14,24 @@ setup(
                 "http services for the templates of a plone site",
     long_description=open("README.rst").read() + "\n" +
                      open(os.path.join("docs", "HISTORY.txt")).read(),
-    url="https://svn.eionet.europa.eu/projects/"
-        "Zope/browser/trunk/Products.EEATemplatesService",
-    classifiers=[
-           "Framework :: Plone",
-           "Programming Language :: Python",
-           "Topic :: Software Development :: Libraries :: Python Modules",
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          "Framework :: Zope2",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.0",
+          "Framework :: Plone :: 4.1",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Programming Language :: Zope",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
       ],
-    keywords='EEA Templates Service',
-    author='Sasha Vincic (EEA), European Environment Agency',
-    author_email='webadmin@eea.europa.eu',
+    keywords='EEA Templates Service Add-ons Plone Zope',
+    author='European Environment Agency: IDM2 A-Team',
+    author_email='eea-edw-a-team-alerts@googlegroups.com',
+    url='https://github.com/eea/Products.EEATemplatesService',
     license='GPL',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['Products'],
